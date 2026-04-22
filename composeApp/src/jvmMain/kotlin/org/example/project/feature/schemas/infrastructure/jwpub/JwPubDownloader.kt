4c75acfae83a0a66d30bcbbda274b47f6a314cf4
Fabio Rizzello <fabio.rizzello@innovaomnia.it>
2026-04-22 13:04:31 +0200

    refactor(schemas): swap deprecated ktor readBytes for readRawBytes in JwPubDownloader

--- composeApp/src/jvmMain/kotlin/org/example/project/feature/schemas/infrastructure/jwpub/JwPubDownloader.kt
+++ composeApp/src/jvmMain/kotlin/org/example/project/feature/schemas/infrastructure/jwpub/JwPubDownloader.kt
@@ -3,7 +3,7 @@ package org.example.project.feature.schemas.infrastructure.jwpub
 import arrow.core.Either
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.statement.readBytes
+import io.ktor.client.statement.readRawBytes
 import org.example.project.core.domain.DomainError
 
 class JwPubDownloader(private val httpClient: HttpClient) {
@@ -13,6 +13,6 @@ class JwPubDownloader(private val httpClient: HttpClient) {
         if (response.status.value !in 200..299) {
             throw java.io.IOException("Download $url: HTTP ${response.status.value}")
         }
-        response.readBytes()
+        response.readRawBytes()
     }.mapLeft { DomainError.Network(it.message ?: "Errore download jwpub") }
 }
